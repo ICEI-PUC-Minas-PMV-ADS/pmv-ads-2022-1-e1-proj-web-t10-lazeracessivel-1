4e9a46435f969398868374446e8e4f0643698544
Pedro Ertal <53317747+pedroertal@users.noreply.github.com>
2022-06-12 12:53:52 -0300

    Delete config.php

--- GitHub Pedro/LazerAcessivel/LA-registro/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUserName = 'root';
-    $dbPassword = '';
-    $dbName = 'lazer-acessivel';
-
-    $conexao = new mysqli($dbHost, $dbUserName, $dbPassword, $dbName);
-?>
