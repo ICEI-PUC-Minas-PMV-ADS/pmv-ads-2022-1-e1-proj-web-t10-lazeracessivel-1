5eefbbff796fec720c92a3ae8ed7d3ef6bf6eb63
Pedro Ertal <53317747+pedroertal@users.noreply.github.com>
2022-06-26 15:27:19 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@ function logar() {
     if( email.value == localStorage.getItem("email") && senha.value == localStorage.getItem("senha")){
         localStorage.setItem("acesso", true)
         alert("Autenticado com sucesso")
-        window.location.href = '../index.html'
+        window.location.href = './index.html'
     }
     else{
         alert("Usuário ou senha inválidos.")
