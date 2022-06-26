f09f32d038466a3e4a1d62abd5e59d79e13e118f
Sarah Ellem <89920953+SarahEllem@users.noreply.github.com>
2022-06-26 14:33:04 -0300

    Add files via upload

---
+++ assets/js/menuResponsivo.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navResponsive").style.width = '100%';
+}
+
+function closeNav() {
+   document.getElementById("navResponsive").style.width = '0%';
+}
